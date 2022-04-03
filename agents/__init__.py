edf73ad8eddf3e9d2c6f3e066bc8cebcea1a0eea
Chang He <chang.he2@mail.mcgill.ca>
2022-04-03 19:05:42 -0400

    ...

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,8 +3,6 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .chang_agent import ChangAgent
-<<<<<<< HEAD
-=======
 
 from .lin_agent import LinAgent
 
@@ -12,4 +10,3 @@ from .mcts_agent import MCTSAgent
 # temp class
 
 
->>>>>>> 1853ee99e4c056aafc7acbb9f227ca536762c661
