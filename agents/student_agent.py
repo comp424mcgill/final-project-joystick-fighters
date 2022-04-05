23a64bde54ffcb083d80eceb0bccd15d32cb7535
Chang He <chang.he2@mail.mcgill.ca>
2022-04-05 19:24:10 -0400

    this is a commit

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -27,10 +27,7 @@ class StudentAgent(Agent):
         }
         self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
         self.opposites = {0: 2, 1: 3, 2: 0, 3: 1}
-        self.max_my_reach = 2
-        self.max_adv_reach = 2
-        self.my_barrier_score = np.array([-3,-2,-1,-1,-1])
-        self.adv_barrier_score = np.array([5000,400,1,1,1])
+        self.root_node = None
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
@@ -48,8 +45,41 @@ class StudentAgent(Agent):
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
         # dummy return
+        start_time = time.time()
+        list_new_board, list_new_pos, list_new_dir = self.all_next_state(chess_board, my_pos, adv_pos, True)
+        list_rm = []
+        for i in range(len(list_new_board)):
+            end_result, end_score = self.check_endgame(list_new_board[i], list_new_pos[i], adv_pos)
+            if (end_result, end_score) == (True, 1): # if the position is winning return this
+                return list_new_pos[i], list_new_dir[i]
+            elif (end_result, end_score) == (True, 0):
+                list_rm.append(i)
+            elif (end_result, end_score) == (True, 0.5):
+                continue
+            '''
+            else:
+                list_adv_new_board, list_adv_new_pos, list_adv_new_dir = self.all_next_state(list_new_board[i], list_new_pos[i], adv_pos, False)
+                for j in range(len(list_adv_new_board)):
+                    adv_result, adv_score = self.check_endgame(list_adv_new_board[j], list_adv_new_pos[j], list_new_pos[i])
+                    if (adv_result, adv_score) == (True, 1):
+                        list_rm.append(i)
+                        break
+            '''
+        list_rm.reverse()
+        for k in range(len(list_rm)): # removing losing position
+            list_new_board.pop(list_rm[k])
+            list_new_pos.pop(list_rm[k])
+            list_new_dir.pop(list_rm[k])
+        zip_list = list(zip(list_new_board, list_new_pos, list_new_dir))
+        np.random.shuffle(zip_list)
+        shuffle_new_board, shuffle_new_pos, shuffle_new_dir = zip(*zip_list)
+        shuffle_new_board = np.array(shuffle_new_board)
+        shuffle_new_pos = list(shuffle_new_pos)
+        shuffle_new_dir = list(shuffle_new_dir)
+        end_time = time.time()
+        print(end_time-start_time)
         
-        return self.greedy_decision(chess_board, my_pos, adv_pos)
+        return shuffle_new_pos[0], shuffle_new_dir[0]
     
     def check_endgame(self, board, my_pos, adv_pos):
         father = dict()
@@ -187,8 +217,8 @@ class StudentAgent(Agent):
                 list_new_dir.append(dir)
         return list_new_board, list_new_pos, list_new_dir
     
-    # given current state, evaluate each position and return the best state with highest heuristic score
-    def greedy_decision(self, board, my_pos, adv_pos):
+    # given current state, return the winning state if any, else delete all losing state and shuffle the list
+    def preprocess_next_state(self, board, my_pos, adv_pos):
         list_new_board, list_new_pos, list_new_dir = self.all_next_state(board, my_pos, adv_pos, True)
         list_rm = []
         for i in range(len(list_new_board)):
@@ -213,59 +243,10 @@ class StudentAgent(Agent):
             list_new_board.pop(list_rm[k])
             list_new_pos.pop(list_rm[k])
             list_new_dir.pop(list_rm[k])
-        # we prefer:
-        # position that is far away from wall and adv near wall
-        # my_reach: get the number of barrier within the reach as a score, lower the better
-        # adv_reach: get the number of barrier within the adversary's reach as a score, higher the better
-        state_score = np.array([])
-        #print(len(list_new_board))
-        board_size = list_new_board[0].shape[0]
-        for i in range(len(list_new_board)):
-            score_i = 0
-            r, c = list_new_pos[i]
-            for reach_row in range(self.max_my_reach*2):
-                for reach_col in range(self.max_my_reach*2):
-                    if (c-self.max_my_reach-reach_col)<0 or (c-self.max_my_reach+reach_col)>=board_size or (r-self.max_my_reach-reach_row)<0 or (r-self.max_my_reach+reach_row)>=board_size:
-                            score_i -= 10
-                    if (r-self.max_my_reach-reach_row)>=0 and (r-self.max_my_reach+reach_row)<board_size:
-                        if (c-self.max_my_reach-reach_col)>=0 and (c-self.max_my_reach+reach_col)<board_size:
-                            score_i += self.my_barrier_score[np.abs(reach_row-self.max_my_reach)+np.abs(reach_col-self.max_my_reach)]*np.count_nonzero((list_new_board[i])[r-self.max_my_reach, c-self.max_my_reach])
-            ra, ca = adv_pos
-            if (r-ra)>0:
-                if (c-ca)>0:
-                    if list_new_dir[i]==0 or list_new_dir[i]==3:
-                        score_i += 2
-                elif (c-ca)==0:
-                    if list_new_dir[i]==0:
-                        score_i += 2
-                else:
-                    if list_new_dir[i]==0 or list_new_dir[i]==1:
-                        score_i += 2
-            elif (r-ra)==0:
-                if (c-ca)>0:
-                    if list_new_dir[i]==3:
-                        score_i += 2
-                elif (c-ca)<0:
-                    if list_new_dir[i]==1:
-                        score_i += 2
-            elif (r-ra)<0:
-                if (c-ca)>0:
-                    if list_new_dir[i]==2 or list_new_dir[i]==3:
-                        score_i += 2
-                elif (c-ca)==0:
-                    if list_new_dir[i]==2:
-                        score_i += 2
-                else:
-                    if list_new_dir[i]==1 or list_new_dir[i]==2:
-                        score_i += 2
-            #for reach_row in range(self.max_adv_reach*2):
-                #if (r-self.max_adv_reach-reach_row)>=0 and (r-self.max_adv_reach+reach_row)<board_size:
-                    #for reach_col in range(self.max_adv_reach*2):
-                        #if (c-self.max_adv_reach-reach_col)>=0 and (c-self.max_adv_reach+reach_col)<board_size:
-                            #score_i += self.adv_barrier_score[np.abs(reach_row-self.max_adv_reach)+np.abs(reach_col-self.max_adv_reach)]*np.count_nonzero((list_new_board[i])[r-self.max_adv_reach, c-self.max_adv_reach])
-            state_score = np.append(state_score, score_i)
-        best_idx = (np.argwhere(state_score==np.amax(state_score))).ravel()
-        rand_idx = best_idx[np.random.randint(len(best_idx))]
-        best_pos = list_new_pos[rand_idx]
-        best_dir = list_new_dir[rand_idx]
-        return best_pos, best_dir
+        zip_list = list(zip(list_new_board, list_new_pos, list_new_dir))
+        np.random.shuffle(zip_list)
+        shuffle_new_board, shuffle_new_pos, shuffle_new_dir = zip(*zip_list)
+        shuffle_new_board = np.array(shuffle_new_board)
+        shuffle_new_pos = list(shuffle_new_pos)
+        shuffle_new_dir = list(shuffle_new_dir)
+        return shuffle_new_board, shuffle_new_pos, shuffle_new_dir
